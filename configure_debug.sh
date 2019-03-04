7ebadb1bf8e1cab04be5a5eae2fa38d34472432a
Martin Bertsche <martin72216@googlemail.com>
2019-03-04 16:49:04 +0100

    added shorthands for configuration

---
+++ configure_debug.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+meson debug --cross-file cross/arm-none-eabi-crossfile.txt --buildtype debug
---
+++ configure_release.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+meson release --cross-file cross/arm-none-eabi-crossfile.txt --buildtype release
